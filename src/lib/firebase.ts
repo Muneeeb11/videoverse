cfaac13c8549a9fa466f589ffef74629ae22d0fd
Muneeb Ur Rehman <muneebulrehman12345@gmail.com>
2025-08-28 04:05:20 +0000

    i am saying fourth time still same issues. and i didnt paste any rules b

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,12 +8,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyBcr9mv3RwCLX8HrSyETyZ9ZDfv-4xx74U",
+  authDomain: "videoverse-k90qc.firebaseapp.com",
+  projectId: "videoverse-k90qc",
+  storageBucket: "videoverse-k90qc.firebasestorage.app",
+  messagingSenderId: "1007523379046",
+  appId: "1:1007523379046:web:d7158172ecfddd31e83aa7"
 };
 
 // Initialize Firebase
